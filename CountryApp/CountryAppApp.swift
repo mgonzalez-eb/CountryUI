8cd3f350c968927260053f3d710efe65c75fcad8
mgonzalez-eb <mgonzalez@eventbrite.com>
2021-08-13 14:21:04 -0300

    Initial Commit

---
+++ CountryApp/CountryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryAppApp.swift
+//  CountryApp
+//
+//  Created by mgonzalez on 13/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct CountryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
